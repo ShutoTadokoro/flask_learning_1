68c2d3f86d20775f293cc987eef63db25f1e400d
Shuto Tadokoro <tadosyu@gmail.com>
2022-10-03 20:09:00 +0900

    編集削除機能追加

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
+from operator import methodcaller
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
@@ -14,6 +15,38 @@ class Post(db.Model):
     body = db.Column(db.String(300), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+
+@app.route("/create", methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        body = request.form.get('body')
+        post = Post(title=title, body=body)
+        db.session.add(post)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('create.html')
+
+@app.route("/<int:id>/update", methods=['GET','POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html',post=post)
+    else:
+        post.title = request.form.get('title')
+        post.body = request.form.get('body')
+        db.session.commit()
+        return redirect('/')
+
+@app.route("/<int:id>/delete", methods=['GET'])
+def delete(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
